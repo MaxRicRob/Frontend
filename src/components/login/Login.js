4d7dd4de316826a3ee7ce1352e0a12febc0c8bb2
simontree <simonroof@tutanota.com>
2022-05-16 17:02:46 +0200

    added tailwind and login-component

---
+++ src/components/login/Login.js
@@ -0,0 +1,11 @@
+
+
+function Login() {
+  return (
+    <div class="text-center bg-green-200">
+     LoginPage
+    </div>
+  );
+}
+
+export default Login;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import './index.css';
-import App from './App';
+import Login from './components/login/Login'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Login />
   </React.StrictMode>
 );
 
