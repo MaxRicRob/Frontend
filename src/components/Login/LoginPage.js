3e5f3e535694ffd3283e5bc290b66c6af6b02d51
simontree <simonroof@tutanota.com>
2022-05-31 20:53:54 +0200

    minor style changes

--- src/components/Login/LoginPage.js
+++ src/components/Login/LoginPage.js
@@ -5,16 +5,15 @@ function Login() {
     <body class="h-screen bg-stone-300 font-sans">
       <div class="flex shadow-md absolute inset-40 bg-green-300 rounded-lg overflow-hidden">
         <div class="shrink-0">
-          <img class="" src={fruitImg} alt="fruits"/>
+          <img src={fruitImg} alt="fruits"/>
         </div>
-        
         <div class="grid auto-rows-max ml-20 mt-40">
         <p class="mb-10 font-bold text-lg">Welcome to Fruitilicious!</p>
           <div>Username: 
             <input class="border-2 hover:border-green-500 focus:outline-none rounded-md ml-2 mb-5 h-10"></input>
             </div>          
           <div>Password: 
-            <input class="border-2 hover:border-green-500 focus:outline-none rounded-md ml-4 mb-5 h-10"></input>
+            <input class="border-2 hover:border-green-500 focus:outline-none rounded-md ml-3.5 mb-5 h-10"></input>
             </div>
           <div>
             <button class="cursor-pointer rounded-full w-20 h-10 bg-green-500 hover:bg-green-400">Login</button>
