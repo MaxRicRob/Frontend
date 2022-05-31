421d255ecca49722cfa1acbab1894a997a79168a
simontree <simonroof@tutanota.com>
2022-05-31 20:47:09 +0200

    minor style changes

--- src/components/Login/LoginPage.js
+++ src/components/Login/LoginPage.js
@@ -11,13 +11,13 @@ function Login() {
         <div class="grid auto-rows-max ml-20 mt-40">
         <p class="mb-10 font-bold text-lg">Welcome to Fruitilicious!</p>
           <div>Username: 
-            <input class="border-2 hover:border-green-500 rounded-md ml-2 mb-5 h-10"></input>
+            <input class="border-2 hover:border-green-500 focus:outline-none rounded-md ml-2 mb-5 h-10"></input>
             </div>          
           <div>Password: 
-            <input class="border-2 hover:border-green-500 rounded-md ml-4 mb-5 h-10"></input>
+            <input class="border-2 hover:border-green-500 focus:outline-none rounded-md ml-4 mb-5 h-10"></input>
             </div>
           <div>
-            <button class="cursor-pointer rounded-full w-20 h-10 bg-green-500">Login</button>
+            <button class="cursor-pointer rounded-full w-20 h-10 bg-green-500 hover:bg-green-400">Login</button>
             </div>
         </div>
       </div>
